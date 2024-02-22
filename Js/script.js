bc79b9e0875ebd94f465d2581f6f5a6ca4c55750
sarmin27 <sarminsultanamaya2000@gmail.com>
2024-02-22 22:36:15 +0600

    added Grand Total , coupon and continue part.

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,25 @@
+let clicks = 0;
 const seatButton = document.querySelectorAll(".seatButton");
-console.dir(seatButton);
-
 for (const button of seatButton) {
-  button.addEventListener("click", function () {
-    button.classList.replace("bg-slate-100", "bg-green-500");
-    button.classList.replace("text-slate-600", "text-white");
-    const buttonValue = button.textContent;
-    decressAvailable();
-    incressSeatValue();
-    createSeat(buttonValue);
+  button.addEventListener("click", function (event) {
+    event.stopPropagation();
+    if (clicks < 4) {
+      clicks++;
+      button.classList.replace("bg-slate-100", "bg-green-500");
+      button.classList.replace("text-slate-600", "text-white");
+      const buttonValue = button.textContent;
+      decressAvailable();
+      incressSeatValue();
+      createSeat(buttonValue);
 
-    button.disabled = true;
+      const totalAmount = totalPrice();
+      coupon(totalAmount);
+
+      button.disabled = true;
+    } else {
+      button.disabled = true;
+      alert("You cannot click anymore!");
+    }
   });
 }
 
@@ -42,22 +51,83 @@ function createSeat(buttonValue) {
 
   // Create seat Number
   const seatNumberElement = document.createElement("h3");
-  seatNumberElement.classList.add("text-base", "text-gray-500");
+  seatNumberElement.classList.add("text-baseline", "text-gray-500");
   seatNumberElement.id = "seatNumber";
   seatNumberElement.textContent = `${buttonValue}`;
   seatElement.appendChild(seatNumberElement);
 
   // Create Seat type
   const seatTypeElement = document.createElement("h3");
-  seatTypeElement.classList.add("text-base", "text-gray-500");
+  seatTypeElement.classList.add("text-baseline", "text-gray-500");
   seatTypeElement.textContent = "Economy";
   seatElement.appendChild(seatTypeElement);
 
   // Create Price element
   const priceElement = document.createElement("h3");
-  priceElement.classList.add("text-base", "text-gray-500");
+  priceElement.classList.add("text-baseline", "text-gray-500", "price");
   priceElement.textContent = "550";
   seatElement.appendChild(priceElement);
 
   afterSelected.appendChild(seatElement);
 }
+
+function totalPrice() {
+  const prices = document.getElementsByClassName("price");
+  let total = 0;
+  for (const price of prices) {
+    const priceValue = price.innerText;
+    const priceValuenumber = parseFloat(priceValue);
+    total = priceValuenumber + total;
+  }
+
+  const totalAmount = document.querySelector("#totalAmount");
+  totalAmount.textContent = " ";
+  totalAmount.innerText = total;
+  return total;
+}
+
+function coupon(amount) {
+  const couponField = document.getElementById("coupon");
+  const grandTotal = document.querySelector("#grandAmmount");
+  grandTotal.textContent = " ";
+
+  const offerNew = document.querySelector("#new");
+  const offerNewValue = offerNew.innerText;
+
+  const offerCouple = document.querySelector("#couple");
+  const offerCoupleValue = offerCouple.innerText;
+
+  const hideForm = document.querySelector(".form");
+  const condition = document.querySelector("#condition");
+
+  const applyButton = document.querySelector("#apply");
+
+  couponField.addEventListener("change", function (event) {
+    event.stopPropagation();
+    const typedValue = event.target.value;
+
+    applyButton.addEventListener("click", function (event) {
+      event.stopPropagation();
+
+      if (typedValue === offerNewValue) {
+        const afterNew = amount * (15 / 100);
+        const newGrandTotal = amount - afterNew;
+        grandTotal.innerText = newGrandTotal;
+        hideForm.classList.add("hidden");
+        condition.classList.add("hidden");
+      } else if (typedValue === offerCoupleValue) {
+        const afterCouple = amount * (20 / 100);
+        const coupleGrandTotal = amount - afterCouple;
+        grandTotal.innerText = coupleGrandTotal;
+        hideForm.classList.add("hidden");
+        condition.classList.add("hidden");
+      } else {
+        alert("worng Coupon");
+      }
+    });
+  });
+}
+
+document.getElementById("continue").addEventListener("click", function () {
+  location.reload(true);
+});
