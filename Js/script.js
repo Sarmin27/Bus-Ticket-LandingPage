ef9554fc89dc844d210e968b2e209735950a2383
sarmin27 <sarminsultanamaya2000@gmail.com>
2024-02-22 17:59:02 +0600

    added append child

--- Js/script.js
+++ Js/script.js
@@ -5,8 +5,11 @@ for (const button of seatButton) {
   button.addEventListener("click", function () {
     button.classList.replace("bg-slate-100", "bg-green-500");
     button.classList.replace("text-slate-600", "text-white");
+    const buttonValue = button.textContent;
     decressAvailable();
     incressSeatValue();
+    createSeat(buttonValue);
+
     button.disabled = true;
   });
 }
@@ -29,4 +32,32 @@ function incressSeatValue() {
   let count = 0;
   count = seatCountIntiger + 1;
   seatCount.innerText = count;
+}
+
+function createSeat(buttonValue) {
+  const afterSelected = document.querySelector(".afterSelected");
+
+  const seatElement = document.createElement("div");
+  seatElement.classList.add("flex", "justify-between");
+
+  // Create seat Number
+  const seatNumberElement = document.createElement("h3");
+  seatNumberElement.classList.add("text-base", "text-gray-500");
+  seatNumberElement.id = "seatNumber";
+  seatNumberElement.textContent = `${buttonValue}`;
+  seatElement.appendChild(seatNumberElement);
+
+  // Create Seat type
+  const seatTypeElement = document.createElement("h3");
+  seatTypeElement.classList.add("text-base", "text-gray-500");
+  seatTypeElement.textContent = "Economy";
+  seatElement.appendChild(seatTypeElement);
+
+  // Create Price element
+  const priceElement = document.createElement("h3");
+  priceElement.classList.add("text-base", "text-gray-500");
+  priceElement.textContent = "550";
+  seatElement.appendChild(priceElement);
+
+  afterSelected.appendChild(seatElement);
 }
